808db1add618afc441379e8c186765d718ef5956
M <175529974@qq.com>
2015-12-04 14:12:40 +0800

    add

---
+++ WebUI/Controllers/AnimationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AnimationController : Controller
+    {
+        // GET: Animation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
